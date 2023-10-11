02c9cd6eb4080fce9b12072e7c39bd4204e6e92c
athevinha <nguyenthevinh1002@gmail.com>
2023-10-11 08:49:49 +0700

    add sort

---
+++ src/main/java/com/mycompany/app/sort/insertionSort.java
@@ -0,0 +1,26 @@
+package com.mycompany.app.sort;
+
+import java.util.Arrays;
+
+public class insertionSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while(j >= 1 && arr[j] > arr[j - 1]){
+
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        sort(arr);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+}
---
+++ src/main/java/com/mycompany/app/sort/mergeSort.java
@@ -0,0 +1,4 @@
+package com.mycompany.app.sort;
+
+public class mergeSort {
+}
